3e2b28f271e99739bb086e1063cef9ad8d230db7
Elvin Nunez <eanunez25@gmail.com>
2020-09-15 23:02:12 -0500

    update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
+
   def index 
     @users = User.all
     @friends = current_user.friends
     @pending_requests = current_user.sent_requests
     @friend_requests = current_user.received_requests
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
 end
