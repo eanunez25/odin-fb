6bf3d9a98f1957a7c2ee023cec422524830bd7c6
Elvin Nunez <eanunez25@gmail.com>
2020-09-15 21:51:55 -0500

    refine friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,7 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
+
+  scope :friends, -> { where('status = ?', true) }
+  scope :not_friends, -> { where('status = ?', false) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :requests_as_requestor, foreign_key: :requestor_id, class_name: "Request"
-  has_many :requests_as_receiver, foreign_key: :receiver_id, class_name: "Request"
+  has_many :sent_requests, foreign_key: :requestor_id, class_name: "Request"
+  has_many :received_requests, foreign_key: :receiver_id, class_name: "Request"
 
-  has_many :friends, class_name: "User", foreign_key: :user_id
-  has_many :friendships
-  has_many :friends, through: :friendships
+  has_many :friends, -> { merge(Friendship.friends) },
+            through: :sent_requests, source: :receiver
+  has_many :pending_requests, -> { merge(Friendship.not_friends) }, 
+            through: :sent_requests, source: :receiver
 
   has_many :notifications
 
